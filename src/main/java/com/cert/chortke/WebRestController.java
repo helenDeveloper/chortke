f8952d58ccf0ce60c6b6d49e6d698d17cee92ef5
helenDeveloper <startpoint1987@gmail.com>
2017-11-06 17:13:32 +0330

    added my github name

--- src/main/java/com/cert/chortke/WebRestController.java
+++ src/main/java/com/cert/chortke/WebRestController.java
@@ -1,12 +1,14 @@
 package com.cert.chortke;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
 
-	@RequestMapping("/api/hi")
+	
+	@GetMapping("/api/hi")
 	public String sayHi() {
 
 		return "hi";
