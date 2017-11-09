8142f13b6924ad997bd3e05510e82c449b616b9e
tahere <tahere@tahere-PC>
2017-11-09 12:54:04 -0800

    this is a commit on save or update service in spring and angular

--- src/main/java/com/cert/chortke/controller/UserController.java
+++ src/main/java/com/cert/chortke/controller/UserController.java
@@ -1,18 +1,19 @@
 package com.cert.chortke.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cert.chortke.entities.UserEntity;
 import com.cert.chortke.service.UserRepository;
 
-@Controller
+@RestController
 @RequestMapping("/")
 public class UserController {
 
@@ -24,8 +25,29 @@ public class UserController {
 
 	}
 
+	@RequestMapping(value = "/api/userlist", method = RequestMethod.GET)
+	public List<UserEntity> getUsers() {
+
+		List<UserEntity> userList = userRepository.findAll();
+
+		if (userList != null)
+			return userList;
+		return new ArrayList<UserEntity>();
+
+	}
+
+	@RequestMapping(value = "/api/saveUser", method = RequestMethod.POST)
+	public Long saveOrUpdate(@PathVariable("user") UserEntity user) {
+
+		UserEntity result = userRepository.save(user);
+		if (result != null)
+			return result.getId();
+		return null;
+	}
+
 	@RequestMapping(value = "/{username}", method = RequestMethod.GET)
-	public String getUserList(@PathVariable("username") String username, Model model) {
+	public String getUserList(@PathVariable("username") String username,
+			Model model) {
 
 		List<UserEntity> userList = userRepository.findByUsername(username);
 
